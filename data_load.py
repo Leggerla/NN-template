f388d8ea2c27a1d8639240d66c675d6e36c32d3f
Lucy Airapetyan <52498629+Leggerla@users.noreply.github.com>
2020-07-31 02:32:05 +0300

    Create data_load.py

---
+++ data_load.py
@@ -0,0 +1,15 @@
+from torch.utils.data import Dataset
+import torch
+
+class MyDataset(Dataset):
+    
+    def __init__(self, ):
+
+        self.len_dataset = 
+    
+    def __len__(self):
+        return self.len_dataset
+    
+    def __getitem__(self, idx):
+        
+        return torch.tensor(), torch.tensor()
